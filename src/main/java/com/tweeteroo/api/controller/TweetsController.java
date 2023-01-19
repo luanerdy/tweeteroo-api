d9b9e7f8a8ca92f5389e6871637e81c6203136d9
luanerdy <luan.vini1@gmail.com>
2023-01-18 22:25:16 -0300

    feat: create DTOs

--- src/main/java/com/tweeteroo/api/controller/TweetsController.java
+++ src/main/java/com/tweeteroo/api/controller/TweetsController.java
@@ -1,8 +1,13 @@
 package com.tweeteroo.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.tweeteroo.api.dto.TweetDTO;
 
 @RestController
 @RequestMapping("/tweets")
@@ -10,5 +15,14 @@ public class TweetsController {
 	@GetMapping
 	public String getAll() {
 		return "All Tweets";
+	}
+
+	@GetMapping("/{id}")
+	public String getById(@PathVariable int id) {
+		return "Tweet " + id;
+	}
+
+	@PostMapping
+	public void create(@RequestBody TweetDTO tweet) {
 	}
 }
---
+++ src/main/java/com/tweeteroo/api/dto/UserDTO.java
@@ -0,0 +1,4 @@
+package com.tweeteroo.api.dto;
+
+public record UserDTO(String username, String avatar) {
+}
